40e44265c892256f52db2ac4ed1f6b534350134b
mrmiguetara <miguelag1999@hotmail.com>
2020-06-21 01:56:15 -0700

    Headlines tab

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  news: Article[] = [];
+
+  categories = [
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology',
+  ];
+
+  constructor( private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.loadHeadlines( this.categories[0] );
+  }
+
+  getHeadlines( event ) {
+    const category = event.detail.value;
+
+    this.news = [];
+
+    this.loadHeadlines(category);
+  }
+
+  private loadHeadlines( category: string) {
+    this.newsService.getTopHeadlinesByCategory(category).subscribe(
+      response => {
+        this.news.push( ...response.articles );
+      }
+    );
+  }
 
 }
